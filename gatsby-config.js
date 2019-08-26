a9a30cfa0bd118b22f076d540885057fd3bc0916
Damien Laureaux <d.laureaux@timoa.com>
2019-08-26 15:05:06 +0200

    Rename the title `Overview` instead of the name of the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	siteMetadata: {
-		title: `Chrome Tab Rotate CMS`,
+		title: `Overview`,
 		author: `Damien Laureaux`,
 		description: `A CMS for the Tab Rotate Chrome extension that consumes the Chrome Tab Rotate Server API`,
 		siteUrl: `https://github.com/timoa/chrome-tab-rotate-cms/`,
